1ff5de667e7c904075a588cb94a9a3d49fae8858
Michael Christie <michael@bizbo.co.nz>
2025-11-11 21:01:07 +1300

    set up test page

--- components/header.tsx
+++ components/header.tsx
@@ -88,6 +88,18 @@ export const Header = () => {
                         </Link>
                       </NavigationMenuLink>
                     </li>
+                    <li>
+                      <NavigationMenuLink asChild>
+                        <Link
+                          href="/test"
+                          className="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
+                        >
+                          <div className="text-sm font-medium leading-none">
+                            Test
+                          </div>
+                        </Link>
+                      </NavigationMenuLink>
+                    </li>                    
                   </ul>
                 </NavigationMenuContent>
               </NavigationMenuItem>
